48172158258bc346d8a0818ec662098da6a64c0c
Ichsan Al Munawwar Patintingan <ichsan.al.ft21@mail.umy.ac.id>
2022-10-17 17:17:55 +0700

    get array element

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -16,9 +16,31 @@ namespace QuickShort
        //number of element in array
         private int n;
 
-            
-        static void Main(string[] args)
+
+        void read()
         {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements\n");
+            }
+        
+        Console.WriteLine("\n===============");
+        Console.WriteLine("Enter Array Elements");
+        Console.WriteLine("\n===============");
+            //get array elements
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
+
     }
 }
