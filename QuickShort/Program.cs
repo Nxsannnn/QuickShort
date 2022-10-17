31d2edd0bce1e5bc9c9b06d64128c6a1e76b0677
Ichsan Al Munawwar Patintingan <ichsan.al.ft21@mail.umy.ac.id>
2022-10-17 16:52:25 +0700

    Add project files.

---
+++ QuickShort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickShort
+{
+    class program
+    {
+        //arraay of integers to hold value
+        private int n;
+
+        void read
+        static void Main(string[] args)
+        {
+        }
+    }
+}
